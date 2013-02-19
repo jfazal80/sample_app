2a6d54bd7db7cefdb9ee055fa318f19cc20a9ca5
Jawad Fazal <jawadfazal@gmail.com>
2013-02-19 22:47:58 +0100

    Updated to new version of tutorial

--- app/controllers/static_pages_controller.rb
+++ app/controllers/static_pages_controller.rb
@@ -1,7 +1,17 @@
 class StaticPagesController < ApplicationController
   def home
+    @title = "Home"
   end
 
+  def contact
+    @title = "Contact"
+  end
+  
+  def about
+    @title = "About"   
+  end
+  
   def help
+    @title = "Help"
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,17 +1,13 @@
 SampleApp::Application.routes.draw do
-  get "static_pages/home"
-
-  get "static_pages/help"
-
   get "users/new"
-  
-  match '/signup',     :to => 'users#new'
 
-  match '/contact',   :to => 'pages#contact'
-  
-  match '/about',     :to => 'pages#about'
-  
-  match '/help',      :to => 'pages#help'
+  root to: 'static_pages#home'
+
+  match '/signup',  to: 'users#new'
+
+  match '/help',    to: 'static_pages#help'
+  match '/about',   to: 'static_pages#about'
+  match '/contact', to: 'static_pages#contact'
  
 
   # The priority is based upon order of creation:
@@ -63,7 +59,7 @@ SampleApp::Application.routes.draw do
 
   # You can have the root of your site routed with "root"
   # just remember to delete public/index.html.
-  root :to => 'pages#home'
+  # root :to => 'pages#home'
 
   # See how all your routes lay out with "rake routes"
 
